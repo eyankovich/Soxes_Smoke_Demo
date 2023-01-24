8e4a82c718c9afbc41baf6a24b3063c7d6fb1186
Egor Yankovich <yahor.yankovich@gmail.com>
2023-01-24 12:32:41 +0100

    Changes: - Added new test method

--- src/test/java/tests/lauchMainPage/LaunchSmokeTest.java
+++ src/test/java/tests/lauchMainPage/LaunchSmokeTest.java
@@ -6,6 +6,11 @@ import tests.base.BaseTest;
 import static constants.Constant.Urls.SOXES_HOME_PAGE;
 public class LaunchSmokeTest extends BaseTest {
     @Test
+    public void clickOnTermsOfUse() {
+        basePage.open(SOXES_HOME_PAGE);
+        soxesHomePage.acceptCookie();
+    }
+    @Test
     public void testLangChanging() {
         basePage.open(SOXES_HOME_PAGE);
         soxesHomePage.changeLanguage();
